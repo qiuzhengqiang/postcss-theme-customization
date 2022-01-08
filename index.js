678a5a5fd3aead8a2566f0dfdec9d2eadaeffafc
qiuzhengqiang <13578575759@163.com>
2022-01-08 14:34:26 +0800

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = (opts = {}) => {
+  // Work with options here
+  return {
+    postcssPlugin: 'postcss-theme-customization',
+    Declaration(decl, {Rule, Declaration}) {
+      if (decl.prop === '-dark-color') {
+        const newDecl = new Declaration({
+          prop: 'color',
+          value: decl.value
+        })
+        decl.root().append(new Rule({
+          selector: `html[data-theme='dark'] ${decl.parent.selector}`,
+          nodes: [newDecl],
+        }))
+      }
+    },
+  }
+}
+
+module.exports.postcss = true
