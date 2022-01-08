e146d2a28505896dd45a5be3a4229eb3e9c33af0
qiuzhengqiang <13578575759@163.com>
2022-01-08 17:03:59 +0800

    for postcss 7

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-module.exports = (opts = {}) => {
-  // Work with options here
-  return {
-    postcssPlugin: 'postcss-theme-customization',
-    Declaration(decl, {Rule, Declaration}) {
+const postcss = require('postcss')
+module.exports = postcss.plugin('postcss-theme-customization', (opts = {}) => {
+  return (root, result) => {
+    root.walkDecls(function(decl) {
       if (decl.prop === '-dark-color') {
-        const newDecl = new Declaration({
+        const newDecl = postcss.decl({
           prop: 'color',
           value: decl.value
         })
-        decl.root().append(new Rule({
+        decl.root().append(postcss.rule({
           selector: `html[data-theme='dark'] ${decl.parent.selector}`,
           nodes: [newDecl],
         }))
       } else if (decl.prop === '-dark-background-color') {
-        const newDecl = new Declaration({
-          prop: 'background-color',
-          value: decl.value
-        })
-        decl.root().append(new Rule({
-          selector: `html[data-theme='dark'] ${decl.parent.selector}`,
-          nodes: [newDecl],
-        }))
-      }
-    },
+      const newDecl = postcss.decl({
+        prop: 'background-color',
+        value: decl.value
+      })
+      decl.root().append(postcss.rule({
+        selector: `html[data-theme='dark'] ${decl.parent.selector}`,
+        nodes: [newDecl],
+      }))
+    }
+    })
   }
-}
-
-module.exports.postcss = true
+})
--- index.test.js
+++ index.test.js
@@ -2,20 +2,6 @@ const postcss = require('postcss')
 
 const plugin = require('./')
 
-async function run(input, output, opts = {}) {
-  let result = await postcss([plugin(opts)]).process(input, {from: undefined})
-  expect(result.css).toEqual(output)
-  expect(result.warnings()).toHaveLength(0)
-}
-
-/* Write tests here
-
-it('does something', async () => {
-  await run('a{ }', 'a{ }', { })
-})
-
-*/
-// index.js
 postcss([plugin]).process(`
 .header[data-v-271ce826] {
   color: #FFF;
